f25fb22460d49d754e861d7e863ab39b2e258f95
Konstantin <k.shukliar@gmail.com>
2019-08-05 00:31:58 +0300

    try!

--- boom-boost/js/project.js
+++ boom-boost/js/project.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*hamburger*/
 (function() {
 
@@ -29,36 +28,4 @@
     fade: true,
     cssEase: 'linear',
     arrow: true,
-=======
-/*hamburger*/
-(function() {
-
-    "use strict";
-
-    var toggles = document.querySelectorAll(".hamburger-button");
-
-    for (var i = toggles.length - 1; i >= 0; i--) {
-        var toggle = toggles[i];
-        toggleHandler(toggle);
-    };
-
-    function toggleHandler(toggle) {
-        toggle.addEventListener("click", function(e) {
-            e.preventDefault();
-            (this.classList.contains("is-active") === true) ? this.classList.remove("is-active"): this.classList.add("is-active");
-        });
-    }
-
-})();
-
-/*slider*/
-
-
-/*$('.slider').slick({
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear',
-    arrow: true,
->>>>>>> master
 });
