f7e694c42b0f827ca52c84d399859e2897509e7b
pasabay <48209931+pasabay@users.noreply.github.com>
2019-08-04 23:49:23 +0300

    Add files via upload
    снова шляпа

---
+++ boom-boost/js/project.js
@@ -0,0 +1,31 @@
+/*hamburger*/
+(function() {
+
+    "use strict";
+
+    var toggles = document.querySelectorAll(".hamburger-button");
+
+    for (var i = toggles.length - 1; i >= 0; i--) {
+        var toggle = toggles[i];
+        toggleHandler(toggle);
+    };
+
+    function toggleHandler(toggle) {
+        toggle.addEventListener("click", function(e) {
+            e.preventDefault();
+            (this.classList.contains("is-active") === true) ? this.classList.remove("is-active"): this.classList.add("is-active");
+        });
+    }
+
+})();
+
+/*slider*/
+
+
+/*$('.slider').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrow: true,
+});
